9fe6e8b8cb4c12dc3dc7e3e71253cd1bd64b58cc
JJHH123 <125659464+JJHH123@users.noreply.github.com>
2023-02-17 21:28:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const PORT = 5000 || process.env.PORT;
@@ -13,14 +14,15 @@ const multer = require('multer');
 const uploadMiddleware = multer({dest:'uploads/'});
 const fs = require('fs');
 
-app.use(cors({credentials:true, origin:'https://bloggo-nz7v.onrender.com'}));
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
 app.use(express.json())
 app.use(cookieParser())
 const salt = bcrypt.genSaltSync(10)
-const secret = 'abc123xyz'
+const secret = process.env.JWT_SECRET
 app.use('/uploads', express.static(__dirname + '/uploads'))
+const uri = process.env.MONGO_URI
 
-mongoose.connect(`mongodb+srv://BLOGGO123:wD0JgC0h7cw5obuL@bloggo-cluster.dr2synp.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(uri)
 .then(() => console.log("connected"))
 .catch(e => console.log(e))
 
@@ -28,6 +30,8 @@ mongoose.connect(`mongodb+srv://BLOGGO123:wD0JgC0h7cw5obuL@bloggo-cluster.dr2syn
 app.get('/test', (req,res) => {
     res.json('ok')
 })
+
+
 
 app.post('/user/register', asyncHandler(async(req,res) => {
     const { username, password } = req.body;
@@ -39,6 +43,8 @@ app.post('/user/register', asyncHandler(async(req,res) => {
         res.status(400).json(e);
     }
 }))
+
+
 
 
 app.post('/user/login', async(req,res) => {
@@ -64,6 +70,8 @@ app.post('/user/login', async(req,res) => {
 })
 
 
+
+
 app.get('/user/profile', asyncHandler(async(req,res) => {
     const { token } = req.cookies;
     if(token === undefined || token.length === 0) res.json('false')
@@ -83,6 +91,8 @@ app.get('/user/profile', asyncHandler(async(req,res) => {
 app.post('/user/logout', async(req,res) => {
     res.cookie('token','').json('ok')
 })
+
+
 
 
 // handle new post
@@ -112,6 +122,8 @@ app.post('/post/newpost', uploadMiddleware.single('file'), async(req,res) => {
 })
 
 
+
+
 app.get('/allposts', async(req,res) => {
     const posts = await Post.find().populate('author', [ 'username' ])
     .sort({createdAt: -1})
@@ -120,10 +132,47 @@ app.get('/allposts', async(req,res) => {
 })
 
 
+
+
 app.get('/post/:id', async(req,res) => {
     const {id} = req.params;
     const postData = await Post.findById(id).populate('author', ['username']);
     res.json(postData)
+})
+
+
+
+// update post
+app.put('/post/update',uploadMiddleware.single('file'), async(req,res) => {
+    let newPath = null;
+    if (req.file) {
+      const {originalname,path} = req.file;
+      const parts = originalname.split('.');
+      const ext = parts[parts.length - 1];
+      newPath = path+'.'+ext;
+      fs.renameSync(path, newPath);
+    }
+  
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, async (err,info) => {
+      if (err) throw err;
+      const {id,title,summary,content} = req.body;
+      const postDoc = await Post.findById(id);
+      const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+      if (!isAuthor) {
+        return res.status(400).json('not authorized');
+      }
+      await postDoc.updateOne({
+        title,
+        summary,
+        content,
+        cover: newPath ? newPath : postDoc.cover,
+      });
+  
+      res.json(postDoc);
+    });
+  
+    
 })
 
 app.listen(PORT, () => {
